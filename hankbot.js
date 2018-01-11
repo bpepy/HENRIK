64fe3f0fe9a2908d7d91809f997cd049c79c23de
bpepy <33704441+bpepy@users.noreply.github.com>
2018-01-11 18:35:56 -0500

    Update hankbot.js

--- hankbot.js
+++ hankbot.js
@@ -5,10 +5,41 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+function wordInString(s, word){
+  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
+}
+
+//wordInString('did you, or did you not, get why?', 'you')
+
+client.on('message', function (user, userID, channelID, message, evt) {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+var hank = 'HENRIK!';
+var mess=message;
+var ishank = Object.is(hank, user);
+
+switch(ishank) {
+
+	case true:
+	break;
+	case false:
+
+    if (message.substring(0, 1) == 'H') {
+        var args = message.substring(1,6);
+        var cmd = args[0];      
+
+        switch(args) {
+            // !ping
+            case 'ENRIK':
+                client.sendMessage({
+                    to: channelID,
+                    message: 'HENRIK!'
+                 });
+            break;
+            
+         }
+     }
+	}
 });
 
 // THIS  MUST  BE  THIS  WAY
