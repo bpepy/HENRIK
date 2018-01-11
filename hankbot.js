1213e1624abc5087490742a29858d859e7b40209
bpepy <33704441+bpepy@users.noreply.github.com>
2018-01-11 18:24:36 -0500

    Update hankbot.js

--- hankbot.js
+++ hankbot.js
@@ -1,25 +1,18 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth_hank.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
 
 function wordInString(s, word){
   return new RegExp( '\\b' + word + '\\b', 'i').test(s);
@@ -27,7 +20,7 @@ function wordInString(s, word){
 
 //wordInString('did you, or did you not, get why?', 'you')
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
 var hank = 'HENRIK!';
