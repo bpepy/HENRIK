3d4529060a01e9daa9562fa9bbfc388e549346d0
bpepy <33704441+bpepy@users.noreply.github.com>
2018-01-13 02:50:13 -0500

    Update hankbot.js

--- hankbot.js
+++ hankbot.js
@@ -1,24 +1,8 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth_hank.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-
-
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
 function wordInString(s, word){
@@ -27,7 +11,7 @@ function wordInString(s, word){
 
 //wordInString('did you, or did you not, get why?', 'you')
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
 var hank = 'HENRIK!';
@@ -47,7 +31,7 @@ switch(ishank) {
         switch(args) {
             // !ping
             case 'ENRIK':
-                bot.sendMessage({
+                client.sendMessage({
                     to: channelID,
                     message: 'HENRIK!'
                  });
@@ -57,3 +41,6 @@ switch(ishank) {
      }
 	}
 });
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
