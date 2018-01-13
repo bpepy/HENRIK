525e076128a1da52f1b0e03bf770cccbbf082be5
bpepy <33704441+bpepy@users.noreply.github.com>
2018-01-13 03:26:11 -0500

    Update hankbot.js

--- hankbot.js
+++ hankbot.js
@@ -5,33 +5,10 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', function (user, userID, channelID, message, evt) {
-var hank = 'HENRIK!';
-var mess=message;
-var ishank = Object.is(hank, user);
-	function wordInString(s, word){
-  return new RegExp( '\\b' + word + '\\b', 'i').test(s);
-}
-
-switch(ishank) {
-
-	case true:
-	break;
-	case false:
-
-    if (message.substring(0, 1) == 'H') {
-        var args = message.substring(1,6);
-        var cmd = args[0];      
-
-        switch(args) {
-            // !ping
-            case 'ENRIK':
-                message.send('HENRIK!');
-            break;
-            
-         }
-     }
-  }
+client.on('message', message => {
+    if (message.content === 'HENRIK') {
+    	send('HENRIK!');
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
