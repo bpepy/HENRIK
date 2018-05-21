2a8db3b7b385754a470df16fd738836b05a28435
bpepy <33704441+bpepy@users.noreply.github.com>
2018-05-21 18:22:05 -0400

    Update hankbot.js

--- hankbot.js
+++ hankbot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'HENRIK!') {
+    if (message.content === 'HENRIK') {
     	message.channel.send('HENRIK!');
   	}
 });
