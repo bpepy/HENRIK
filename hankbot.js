ec1cb1656ad95dd769b1305d745ec85a34cb1ca6
Brian Pepe <brianpepe6@gmail.com>
2022-04-29 15:22:40 -0400

    Update hankbot.js

--- hankbot.js
+++ hankbot.js
@@ -20,9 +20,6 @@ client.on('message', message => {
 	else if (message.content.substring(0,4) === 'IGOR') {
 		message.channel.send('IGOR!');
   	    }    
-	else if (message.content.substring(0,5) === 'HUSKA') {
-		message.channel.send('HUSKA!');
-  	    }   
             break;    
     }
 });
